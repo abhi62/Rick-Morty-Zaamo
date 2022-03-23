cda3538d7dbba25db28b830e56c3e2b258b2f6af
abhi62 <abhi.leon.93@gmail.com>
2022-03-23 20:34:34 +0530

    update for pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+  },
   reactStrictMode: true,
   images: {
-    domains: ['rickandmortyapi.com'],
+    domains: ["rickandmortyapi.com"],
   },
-}
+});
